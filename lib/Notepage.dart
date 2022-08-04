ba06d889e6d303e5bf43385c047deb4cec9f2cc9
Jackcool81 <76261988+Jackcool81@users.noreply.github.com>
2022-08-04 09:29:31 -0700

    2

--- lib/Notepage.dart
+++ lib/Notepage.dart
@@ -1,6 +1,6 @@
 /*import 'dart:html';*/
 
-import 'package:firebase_database/firebase_database.dart';
+// import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:musicproject2/Settings.dart';
 
@@ -18,27 +18,27 @@ class MyPage32 extends StatefulWidget {
 class _MyNext22State extends State<MyPage32> {
   List<TaskCardWidget> data = [];
   void initState(){
-    DatabaseReference _db1 = FirebaseDatabase.instance.ref();
-    var ref = _db1.child('tasks');
-    ref.onValue.listen((event) {
-      //i = 0;
-      int x = event.snapshot.children.length;
-      print(x);
-      data.clear();
-      int t = 0;
-      for (DataSnapshot i in event.snapshot.children){
-        // title = event.snapshot.children.elementAt(i).value.toString();
-
-        setState(() => data.insert(t, (TaskCardWidget(
-          i.children.elementAt(0).value.toString(),
-          i.children.elementAt(1).value.toString(),
-          i.children.elementAt(2).value.toString(),
-        ))));
-        // data[t] = TaskCardWidget(i.children.elementAt(1).value.toString(), i.children.elementAt(0).value.toString());
-        t++;
-      };
-    });
-    super.initState();
+    // DatabaseReference _db1 = FirebaseDatabase.instance.ref();
+    // var ref = _db1.child('tasks');
+    // ref.onValue.listen((event) {
+    //   //i = 0;
+    //   int x = event.snapshot.children.length;
+    //   print(x);
+    //   data.clear();
+    //   int t = 0;
+    //   for (DataSnapshot i in event.snapshot.children){
+    //     // title = event.snapshot.children.elementAt(i).value.toString();
+    //
+    //     setState(() => data.insert(t, (TaskCardWidget(
+    //       i.children.elementAt(0).value.toString(),
+    //       i.children.elementAt(1).value.toString(),
+    //       i.children.elementAt(2).value.toString(),
+    //     ))));
+    //     // data[t] = TaskCardWidget(i.children.elementAt(1).value.toString(), i.children.elementAt(0).value.toString());
+    //     t++;
+    //   };
+    // });
+    // super.initState();
 
   }
   @override
